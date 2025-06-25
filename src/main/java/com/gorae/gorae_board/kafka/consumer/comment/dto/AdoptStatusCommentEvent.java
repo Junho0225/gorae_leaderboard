548319c875f219bc4970d1d07925755213c91136
kaebalsaebal <ssassaium@gmail.com>
2025-06-25 18:37:13 +0900

    add adoptstatusconsumer

---
+++ src/main/java/com/gorae/gorae_board/kafka/consumer/comment/dto/AdoptStatusCommentEvent.java
@@ -0,0 +1,13 @@
+package com.gorae.gorae_board.kafka.consumer.comment.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AdoptStatusCommentEvent {
+    public static final String TOPIC = "adopt-comment-status";
+
+    private String commentId;//채택된 답변
+    private String adoptUserId;//채택된 답변의 유저 Id
+}
